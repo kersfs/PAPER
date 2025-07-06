748b29cbfaa60ca5eab3e43140f521bd5c82d238
KERS <kers@killove.cn>
2025-07-06 22:00:08 +0800

    update V1.3 更新支持云端上传，云端同步 关键词自动学习 增加关键词过滤功能 修改延迟检测和动态休眠为crul延迟，提高稳定性 增加屏幕熄灭检测，屏幕关闭进入休眠 增加电量和温度检测，电量过低进入低功耗模式，温度异常进入休眠 增加标签自动翻译，标签自动更新 支持对参数动态调控

--- Cores/Function/is_downloaded.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#VERSION="1.0.2"
-#初始化数据库
-is_downloaded() {
-    sqlite3 "$DB_FILE" "SELECT 1 FROM downloaded WHERE url='$1'" | grep -q 1
-}
--- Cores/Modules/database.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 初始化数据库
-#VERSION="1.0.2"
-if [ -f "$DB_FILE" ]; then
-    sqlite3 "$DB_FILE" "CREATE TABLE IF NOT EXISTS downloaded (url TEXT PRIMARY KEY);"
-fi
